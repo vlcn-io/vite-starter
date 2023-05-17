4fc65bec689d41c665f5a227a26986e6459a988a
Matt <1009003+tantaman@users.noreply.github.com>
2023-05-17 11:11:29 -0400

    include vulcan logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import vlcnLogo from './assets/vlcn.png'
 import './App.css'
 
 function App() {
@@ -15,8 +16,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://vlcn.io" target="_blank">
+          <img src={vlcnLogo} className="logo react" alt="Vulcan logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Vulcan</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +30,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite, React and Vulcan logos to learn more
       </p>
     </>
   )
