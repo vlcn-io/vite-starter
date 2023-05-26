05f094db6d6e01d51289a291aea8db5cfffde612
Matt <1009003+tantaman@users.noreply.github.com>
2023-05-26 09:57:05 -0400

    new versions of packages

--- src/schemas/main.mjs
+++ src/schemas/main.mjs
@@ -2,7 +2,7 @@ export default {
   namespace: "default",
   name: "main",
   active: true,
-  content: `
+  content: /*sql*/ `
     CREATE TABLE IF NOT EXISTS test (id PRIMARY KEY, name TEXT);
     SELECT crsql_as_crr('test');
   `,
