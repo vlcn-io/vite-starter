630a20e2511ec19bb7af9b05b52afa2672bba6d7
Matt <1009003+tantaman@users.noreply.github.com>
2023-06-23 07:23:01 -0700

    add PWA config for use while offline

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,21 @@ export default defineConfig({
       target: "esnext",
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        globPatterns: [
+          "**/*.js",
+          "**/*.css",
+          "**/*.svg",
+          "**/*.html",
+          "**/*.png",
+          "**/*.wasm",
+        ],
+      },
+    }),
+  ],
   server: {
     fs: {
       strict: false,
