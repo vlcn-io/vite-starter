aa087b4a10cbbe5036e24d563e6cf104a7bea5ed
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-31 06:18:32 -0400

    configure server to use litefs

--- server.js
+++ server.js
@@ -2,24 +2,32 @@ import express from "express";
 import ViteExpress from "vite-express";
 import { attachWebsocketServer } from "@vlcn.io/ws-server";
 import * as http from "http";
+import {
+  createLiteFSDBFactory,
+  createLiteFSWriteService,
+} from "@vlcn.io/ws-litefs";
 
 const PORT = parseInt(process.env.PORT || "8080");
 
 const app = express();
 const server = http.createServer(app);
 
-attachWebsocketServer(server, {
+const litefsConfig = {
+  port: 9000,
+  primaryFileDir: "/var/lib/litefs",
+  primaryFile: ".primary",
+};
+const wsConfig = {
   dbFolder: "./dbs",
   schemaFolder: "./src/schemas",
   pathPattern: /\/sync/,
-});
+};
+const dbFactory = await createLiteFSDBFactory(litefsConfig);
+const dbCache = attachWebsocketServer(server, wsConfig, dbFactory);
+createLiteFSWriteService(litefsConfig, wsConfig, dbCache);
 
 server.listen(PORT, () =>
-<<<<<<< HEAD
   console.log("info", `listening on http://localhost:${PORT}!`)
-=======
-  console.log("info", `listening on https://localhost:${PORT}!`)
->>>>>>> f092e00 (websocket sync, new react hooks, use react strict mode)
 );
 
 ViteExpress.bind(app, server);
